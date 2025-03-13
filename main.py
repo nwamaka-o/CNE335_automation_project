54584faf6f4453d55d90cbd3580e4d66a06b2f57
nwamaka-o <Nuokalla@student.rtc.edu>
2025-03-13 02:49:12 -0700

    Update main.py to support server ping

--- main.py
+++ main.py
@@ -2,12 +2,18 @@
 # Justin Ellis
 # CNE 335 Fall
 
+from Server import Server
+
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Nwamaka Okalla")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    server = Server("54.68.76.21")
+
     # TODO - Call Ping method and print the results
+    result = server.ping()  # Call the ping method
+    print(result)  # Print the results
